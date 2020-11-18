7497369cfe3dc0e14c1ed111457c6f89423e764a
Mateusz Podlasin <podlas29@gmail.com>
2020-11-18 22:57:11 +0100

    index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,55 @@
 import React from "react"
-import { Link } from "gatsby"
+import './index.css'
+import 'typeface-tajawal';
+import me from '../images/me.jpg';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+const indexToC = [
+  {
+    name: 'Frontend & Functional Programming',
+    children: [
+      {
+        name: 'twitter',
+        link: 'https://twitter.com/m_podlasin'
+      },
+      {
+        name: 'articles',
+        link: '/articles'
+      }
+    ]
+  },
+  {
+    name: 'Travel & Photography',
+    children: [
+      {
+        name: 'instagram',
+        link: 'https://instagram.com/mpodlasin'
+      }
+    ]
+  }
+]
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+  <div className="flex justify-center items-center h-screen">
+    <div className="border border-indigo-900 p-3 main-borders">
+      <div className="flex items-center mb-3">
+        <img className="w-20 h-20 rounded-full mr-3" src={me} />
+        <h1 className="text-5xl font-extralight text-indigo-900 flex items-center justify-center flex-grow transform translate-y-2">Mateusz Podlasin</h1>
+      </div>
+      <div className="text-center flex flex-col items-center">
+        {indexToC.map(element => (
+          <div className="border-b m-2">
+            <h2 className="text-2xl font-light">{element.name}</h2>
+            <ul className="flex justify-evenly m-2">
+              {element.children.map(subelement => (
+                <li className="font-medium text-indigo-900 text-xl"><a href={subelement.link}>{subelement.name}</a></li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="text-indigo-900 text-lg mt-2 transform translate-y-1.5">hi@mpodlasin.com</div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  </div>
 )
 
 export default IndexPage
