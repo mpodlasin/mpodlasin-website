5ae3a4c56c4f38a10a34712e391f69aa92bd011a
Mateusz Podlasin <podlasin.mateusz@gmail.com>
2026-06-30 14:17:06 +0200

    better menu and github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className={css.wrapper}>
-      <Link href="/demos/orthographic-hero">Orthographic Hero</Link>
+      <h2>Demos:</h2>
+      <Link href="/demos/orthographic-hero">Cube Rain</Link>
       <Link href="/demos/cave-hero">Cave Hero</Link>
+    
+      <p className={css.github}>This website&apos;s source code is avilable at <a className={css.githubLink} target="_blank" href="https://github.com/mpodlasin/mpodlasin-website">GitHub</a></p>
     </div>
   );
 }
